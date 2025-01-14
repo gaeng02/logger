b7beb80d1dc96bbe604ebb994512d006373e3382
gaeng02 <sondin144@gmail.com>
2025-01-14 23:42:01 +0900

    test

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import logger
 
-logger.log("")
+logger.log("Hello World") # Default 
+
+logger.help_level() 
+logger.log("INFO", "Hello World")
+
+logger.set(level = "WARNING")
+logger.set(filepath = "../test_log")
+
+logger.print()
+logger.copy("../copy_log")
